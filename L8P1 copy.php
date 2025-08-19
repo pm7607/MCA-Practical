4a2c50520ff58d2883acad5838cc463457a6387e
Pratik Mehta <premmehta7607@gmail.com>
2025-08-19 23:32:59 +0530

    remove the demo file

--- L8P1 copy.php
+++
@@ -1,9 +0,0 @@
-<!-- WAP to create numeric array and print it. -->
-<?php
-$numbers = array(10, 20, 30, 40, 50);
-print_r($numbers);
-echo "<br> Array elements:<br>";
-foreach ($numbers as $number) {
-    echo $number . "<br>";
-}
-?>
